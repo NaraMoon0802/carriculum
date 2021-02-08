321eaa11ad26ee3c9633216a38cb0ab008ce19cb
NaraMoon0802 <75877923+NaraMoon0802@users.noreply.github.com>
2021-02-08 12:43:59 +0900

    変数とは

--- 2-1/index.php
+++ 2-1/index.php
@@ -4,5 +4,25 @@
 <?php
 echo "PHPの研修";?>
 <html>
-<p>I say that <?php echo "Hello World!!"; ?> </p>
+<p>I say that <?php echo "Hello!!"; ?> </p>
 </html>
+
+<?php
+// index.php
+$message = "Hello World!"; //$は変数の前に絶対必要です。
+echo $message;
+echo '<br>'; 
+//HTMLの<br>を呼び出すことでブラウザのインターフェイスを
+//構成するHTMLを修正する感じになる。→<br>は''で文字列扱い？
+$x = 8;
+$y = 5.55;
+echo $x;
+echo '<br>';
+echo $y;
+echo '<br>';
+
+$message1 = "Good night";
+var_dump($message1);
+var_dump($x);
+var_dump($y);
+?>
