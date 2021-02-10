d8067ae2bd34e0b805a7b63e1dfc729bc6970319
NaraMoon0802 <75877923+NaraMoon0802@users.noreply.github.com>
2021-02-10 16:30:27 +0900

    PHP　演算子

---
+++ 2-1/index-1.php
@@ -0,0 +1,30 @@
+<?php
+echo 2+3;
+echo "<br>";
+echo 5-2;
+echo "<br>";
+echo 6/3;
+echo "<br>";
+echo 5%2;
+echo "<br>";
+echo 2*3;
+echo "<br>";
+$x=1;
+$x++; //$x = $x+1
+echo $x;
+echo "<br>";
+$x--; //$x = $x-1 上から読み込むから下方が最新情報
+echo $x;
+echo "<br>";
+$x+=5;
+echo $x;
+echo "<br>";
+$x*=5; //$x = $x*5
+echo $x;
+echo "<br>";
+$x/=5;
+echo $x;
+echo "<br>";
+$x-=5;
+echo $x;
+?>
