1cc9ff541f01d9c8e009e8953a6de826429e5834
NaraMoon0802 <75877923+NaraMoon0802@users.noreply.github.com>
2021-02-25 09:55:13 +0900

    ２章チェックテスト　訂正（関数）

--- 2-18/answer.php
+++ 2-18/answer.php
@@ -13,23 +13,35 @@ $command=$_POST["command"];
 <p>①の答え</p>
 <p>
 <?php
-if ($number==80){
-    echo "正解！";
-}else echo "残念・・・";
+function result1($n){
+    if ($n==80){
+        echo "正解！";
+    }else echo "残念・・・";
+}
+result1($number);
+
 ?></p>
 <p>②の答え</p>
 <p>
 <?php
-if ($language=="HTML"){
-    echo "正解！";
-}else echo "残念・・・";
+function result2($l){
+    if ($l=="HTML"){
+        echo "正解！";
+    }else echo "残念・・・";
+}
+result2($language);
+
 ?></p>
 <p>③の答え</p>
 <p>
 <?php
-if ($command=="select"){
-    echo "正解！";
-}else echo "残念・・・";
+function result3($c){
+    if ($c=="select"){
+        echo "正解！";
+    }else echo "残念・・・";
+}
+result3($command);
+
 ?></p>
 </div>
 </body>
