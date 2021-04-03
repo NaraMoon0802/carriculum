11c996667350e6492a769c62bc38025b08c5e0a9
NaraMoon0802 <75877923+NaraMoon0802@users.noreply.github.com>
2021-04-03 21:16:41 +0900

    pdo
    namespaceとクラスの整理

--- pdo/db_connect.php
+++ pdo/db_connect.php
@@ -1,18 +1,20 @@
 <?php
 namespace pdo;
+require_once "./pdo2/db_connect.php";
+use pdo\pdo2\PDO_connect;
+//DBの接続設定をしたPDOインスタンスをかえす。
+function db_connect (){
+    try{
+        $pdo = new \PDO (PDO_connect::PDO_DSN, PDO_connect::PDO_USERNAME, PDO_connect::DB_PASSWORD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+        echo "接続が成功しました";
+    }catch(PDOException $e){
+        echo "Error:". $e->getMessage();
+        die();
+    }
+}
+db_connect();
 
-Class PDO_connect{
-    const DB_DATABASE = "yigroupBlog";
-    const DB_USERNAME = "root";
-    const DB_PASSWORD = "root";
-    const PDO_DSN = "mysql:host=localhost; charset=utf8; dbname=".PDO_connect::DB_DATABASE;
-}
-try {
-    $dbh = new \PDO (PDO_connect::PDO_DSN, PDO_connect::DB_USERNAME, PDO_connect::DB_PASSWORD);
-    echo "DBと接続しました。";
-}catch (PDOExeption $e){
-    echo "Error:". $e->getMessage();
-    die(); //明示的な例外キャッチ//暗黙的ー＞set_exception_handler();
-}
 
 ?>
---
+++ pdo/pdo2/db_connect.php
@@ -0,0 +1,18 @@
+<?php
+namespace pdo\pdo2;
+
+Class PDO_connect{
+    const DB_DATABASE = "yigroupBlog";
+    const DB_USERNAME = "root";
+    const DB_PASSWORD = "root";
+    const PDO_DSN = "mysql:host=localhost; charset=utf8; dbname=".PDO_connect::DB_DATABASE;
+}
+// try {
+//     $dbh = new \PDO (PDO_connect::PDO_DSN, PDO_connect::DB_USERNAME, PDO_connect::DB_PASSWORD);
+//     echo "DBと接続しました。";
+// }catch (PDOExeption $e){
+//     echo "Error:". $e->getMessage();
+//     die(); //明示的な例外キャッチ//暗黙的ー＞set_exception_handler();
+// }
+
+?>
