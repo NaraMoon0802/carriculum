9b86cea2b10c85a2566c67cf4a41ceeae93be8a3
NaraMoon0802 <75877923+NaraMoon0802@users.noreply.github.com>
2021-04-03 18:28:35 +0900

    Create exeption.php
    例外処理とは

---
+++ pdo/exeption.php
@@ -0,0 +1,14 @@
+<?php
+function devide($int, $int2){
+    try {
+        if ($int2 == 0){
+            throw new Exception("0で割ることはできません");
+        }
+        return $int/$int2;
+    } catch (Exception $e){
+        echo "例外処理が発生しました<br>";
+        echo $e->getMessage();
+    }
+}
+echo devide(5, 0);
+?>
